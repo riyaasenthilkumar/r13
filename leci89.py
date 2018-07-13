7c37631f64c317c14d6e9aff5f66fa30c1b5adef
riyaasenthilkumar <36299182+riyaasenthilkumar@users.noreply.github.com>
2018-07-13 12:06:58 +0530

    Create leci89.py

---
+++ leci89.py
@@ -0,0 +1,19 @@
+def sortLexo(my_string):
+ 
+   
+    num = my_string.split()
+     
+    
+    num.sort()
+ 
+    
+    for i in num:
+        print( i ) 
+ 
+
+if __name__ == '__main__':
+     
+    my_num= "6789 " \
+              
+    
+    sortLexo(my_num)
